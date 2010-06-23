e9ccea8271d3e8417767e90d682f7550d215a74f
daniga <daniga@users.sourceforge.net>
2010-06-23 15:41:42 +0200

    added new interfaces for PropertyValues.

---
+++ src/edacc/satinstances/ExampleProperty.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edacc.satinstances;
+
+/**
+ *
+ * @author dgall
+ */
+public class ExampleProperty implements SATInstanceProperty {
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Object computeProperty(SATInstance f) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public PropertyValueType<?> getPropertyValueType() {
+        return new IntegerPropertyValueType();
+    }
+
+  
+
+}
