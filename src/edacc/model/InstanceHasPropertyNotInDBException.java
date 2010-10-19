0bab83d01b1e6203db186a7c7357ea5a188df1aa
rretz <Robert@.(none)>
2010-10-19 15:59:06 +0200

    forgot to add the exception

---
+++ src/edacc/model/InstanceHasPropertyNotInDBException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edacc.model;
+
+/**
+ *
+ * @author rretz
+ */
+public class InstanceHasPropertyNotInDBException extends Exception{
+
+    public InstanceHasPropertyNotInDBException() {
+        super();
+    }
+
+}
