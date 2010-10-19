1cc21692546f74c5d0df57ce631211beb304b69d
Daniel Gall <daniga@users.sourceforge.net>
2010-10-19 17:23:23 +0200

    merge

--- src/edacc/model/InstanceHasPropertyNotInDBException.java
+++ src/edacc/model/InstanceHasPropertyNotInDBException.java
@@ -7,18 +7,13 @@ package edacc.model;
 
 /**
  *
-<<<<<<< HEAD
  * @author dgall
  */
 public class InstanceHasPropertyNotInDBException extends Exception {
-=======
- * @author rretz
- */
-public class InstanceHasPropertyNotInDBException extends Exception{
+
 
     public InstanceHasPropertyNotInDBException() {
         super();
     }
->>>>>>> 99f3273d5ac8eac390e0dc0235ccc0df73624f7e
 
 }
