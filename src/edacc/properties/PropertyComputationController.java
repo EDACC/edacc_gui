6278c0e7b6690bed0e30f3747cfba9428e016555
rretz <Robert@.(none)>
2010-12-04 20:58:37 +0100

    improved the ComputationController

--- src/edacc/properties/PropertyComputationController.java
+++ src/edacc/properties/PropertyComputationController.java
@@ -31,6 +31,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Vector;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -46,9 +48,13 @@ public class PropertyComputationController implements Runnable{
     private int jobs;
     private Tasks task;
     private int allJobs;
+    private Condition condition;
+    private Lock lock;
 
-    public PropertyComputationController(Experiment exp, Vector<Property> givenProperties, boolean recompute, Tasks task) throws NoConnectionToDBException, SQLException, PropertyTypeNotExistException, IOException, PropertyNotInDBException, ComputationMethodDoesNotExistException{
+    public PropertyComputationController(Experiment exp, Vector<Property> givenProperties, boolean recompute, Tasks task, Condition condition, Lock lock) throws NoConnectionToDBException, SQLException, PropertyTypeNotExistException, IOException, PropertyNotInDBException, ComputationMethodDoesNotExistException{
+        this.condition = condition;
         this.task = task;
+        this.lock = lock;
         availableProcessors = Runtime.getRuntime().availableProcessors();
         this.task.setOperationName("compute properties");
         this.task.setStatus("initialize the computation");
@@ -59,19 +65,35 @@ public class PropertyComputationController implements Runnable{
             Logger.getLogger(PropertyComputationController.class.getName()).log(Level.SEVERE, null, ex);
         }
         allJobs = resultPropertyQueue.size();
-        this.task.setStatus("compute");
+        task.setStatus("computed " + (allJobs - resultPropertyQueue.size()) + " of " + allJobs + " properties");
+        task.setTaskProgress(((float)(allJobs - resultPropertyQueue.size()))/((float)allJobs));
     }
 
-    public PropertyComputationController(Vector<Instance> instances, Vector<Property> givenProperties){
+    public PropertyComputationController(Vector<Instance> instances, Vector<Property> givenProperties, Condition condition){
+        this.condition = condition;
+        availableProcessors = Runtime.getRuntime().availableProcessors();
+        this.task.setOperationName("compute properties");
+        this.task.setStatus("initialize the computation");
+        this.recompute = recompute;
         availableProcessors = Runtime.getRuntime().availableProcessors();
         createJobQueue(instances, givenProperties);
+        allJobs = instancePropertyQueue.size();
+        task.setStatus("computed " + (allJobs - instancePropertyQueue.size()) + " of " + allJobs + " properties");
+        task.setTaskProgress(((float)(allJobs - instancePropertyQueue.size()))/((float)allJobs));
+    }
+
+    public void run(Tasks task){
+        task.setStatus("compute");
+        this.task = task;
+        run();
     }
 
     @Override
     public void run() {
-
+        task.setOperationName("Property computation");
         for(int i = 0; i < availableProcessors; i++){
             if(instancePropertyQueue != null){
+                
                 if(instancePropertyQueue.isEmpty()){
                     jobs = i;
                     return;
@@ -103,6 +125,8 @@ public class PropertyComputationController implements Runnable{
                 try {
                 new Thread(new PropertyComputationUnit(instancePropertyQueue.take(), this)).start();
                 jobs++;
+                task.setStatus("computed " + (allJobs - instancePropertyQueue.size()) + " of " + allJobs + " properties");
+                task.setTaskProgress(((float)(allJobs - instancePropertyQueue.size()))/((float)allJobs));
             } catch (InterruptedException ex) {
                 Logger.getLogger(PropertyComputationController.class.getName()).log(Level.SEVERE, null, ex);
             }
@@ -111,14 +135,24 @@ public class PropertyComputationController implements Runnable{
                 try {
                 new Thread(new PropertyComputationUnit(resultPropertyQueue.take(), this)).start();
                 jobs++;
+                task.setStatus("computed " + (allJobs - resultPropertyQueue.size()) + " of " + allJobs + " properties");
+                task.setTaskProgress(((float)(allJobs - resultPropertyQueue.size()))/((float)allJobs));
             } catch (InterruptedException ex) {
                 Logger.getLogger(PropertyComputationController.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
         jobs--;
         if(jobs == 0){
-          task.cancel(true);
-          return;
+          task.cancel(true);        
+          lock.lock();
+          try{
+            condition.signal();
+          } finally{
+            lock.unlock();
+          }
+          
+          
+          return;         
         }
 
     }
