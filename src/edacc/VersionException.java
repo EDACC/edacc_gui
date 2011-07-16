696fbcc58136158027eee1bd49b0eaf13729b6a0
uulm-simon <uulm-simon@users.sourceforge.net>
2011-07-17 01:46:45 +0200

    version exception class

---
+++ src/edacc/VersionException.java
@@ -0,0 +1,11 @@
+package edacc;
+
+/**
+ *
+ * @author simon
+ */
+public class VersionException extends Exception {
+    public VersionException(String message) {
+        super(message);
+    }
+}
