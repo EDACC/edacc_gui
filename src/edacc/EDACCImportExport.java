29d9b8ec6d5a75e969fc8be1a530b2d60cac6688
uulm-simon <uulm-simon@users.sourceforge.net>
2013-02-07 12:43:14 +0100

    import/export: log exceptions to log file

--- src/edacc/EDACCImportExport.java
+++ src/edacc/EDACCImportExport.java
@@ -572,7 +572,7 @@ public class EDACCImportExport extends javax.swing.JPanel {
             
             @Override
             public void run() {
-                ex.printStackTrace();
+                EDACCApp.getLogger().logException(ex);
                 JOptionPane.showMessageDialog(EDACCApp.getApplication().getMainFrame(), "Error while loading data: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
             }
         });
